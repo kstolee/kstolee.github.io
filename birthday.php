573fdff1989e1c546f33ebfbe31c278d26452727
kstolee <kstolee@users.noreply.github.com>
2024-03-31 22:03:39 -0400

    Update birthday.php

--- birthday.php
+++ birthday.php
@@ -1,6 +1,18 @@
-<?php
-$birthday = $_GET['birthday'];
-$homepage = $_GET['homepage'];
-echo "Birthday: $birthday<br>Homepage: <a href='$homepage'>click here</a>";
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Information</title>
+</head>
+<body>
+    <h1>User Information</h1>
+    <?php
+    $birthday = $_GET['birthday'];
+    $homepage = $_GET['homepage'];
+    echo "<p>Here is the information you provided:</p>";
+    echo "<p>Birthday: $birthday</p>";
+    echo "<p>Homepage: <a href='$homepage'>click here</a></p>";
+    ?>
+</body>
+</html>
